3916b134137276b42ac11cfe2133abe100091d4c
tslijianguo <591085278@qq.com>
2025-03-26 23:08:17 +0800

    新增查询区块

---
+++ pkg1/queryBlock.go
@@ -0,0 +1,45 @@
+package pkg1
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func queryBlock() {
+	client, err := ethclient.Dial("https://eth-sepolia.g.alchemy.com/v2/<API_KEY>")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blockNumber := big.NewInt(5671744)
+
+	header, err := client.HeaderByNumber(context.Background(), blockNumber)
+	fmt.Println(header.Number.Uint64())     // 5671744
+	fmt.Println(header.Time)                // 1712798400
+	fmt.Println(header.Difficulty.Uint64()) // 0
+	fmt.Println(header.Hash().Hex())        // 0xae713dea1419ac72b928ebe6ba9915cd4fc1ef125a606f90f5e783c47cb1a4b5
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	block, err := client.BlockByNumber(context.Background(), blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(block.Number().Uint64())     // 5671744
+	fmt.Println(block.Time())                // 1712798400
+	fmt.Println(block.Difficulty().Uint64()) // 0
+	fmt.Println(block.Hash().Hex())          // 0xae713dea1419ac72b928ebe6ba9915cd4fc1ef125a606f90f5e783c47cb1a4b5
+	fmt.Println(len(block.Transactions()))   // 70
+	count, err := client.TransactionCount(context.Background(), block.Hash())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(count) // 70
+}
